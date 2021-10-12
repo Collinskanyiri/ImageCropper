f703c23269798c99e727ba3d5ec6becdca5beed3
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-10-12 11:56:34 +0300

    add corrections on urls.py

--- Cropper/urls.py
+++ Cropper/urls.py
@@ -1,12 +1,10 @@
-from django.conf.urls import url
 from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.main, name='mainPage'), 
-    # url(r'^$', views.photo_list, name="photo_list"),
+    path('', views.main, name='mainPage'),
     path('search/', views.search_images, name='searchPage'), 
     path('location/<location>/', views.view_by_location, name='locationPage'), 
 ]
