a6528ed488757638836b2a71f697897ac76334bd
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-10-12 11:41:48 +0300

    add corrections admin.py

--- Cropper/admin.py
+++ Cropper/admin.py
@@ -1,3 +1,10 @@
+from .models import *
 from django.contrib import admin
+# from .models import Photo
 
 # Register your models here.
+
+# admin.site.register(Photo)
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image)
